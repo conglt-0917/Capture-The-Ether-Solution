8e24ebe1029338439f64484bce5cacf7cc165126
kedumuc1712 <sieutroma7k51@gmail.com>
2019-07-01 00:15:10 +0700

    guess random number with getStorageAt

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const Web3 = require('web3');
 const rpc = 'https://ropsten.infura.io/v3/4723644fef534b01b9fbf52d7eb2fc1e';
 const web3 = new Web3(rpc);
 
-// adress of contract deploy
+
 web3.eth.getStorageAt('0x40766010F9f98d76d2d2d28536b07821758Bf365', 0).then(result => {
   console.log(web3.utils.toDecimal(result));
 });
